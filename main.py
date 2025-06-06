0799dbcabe9efd90a69194b2b8fb3bf6489b846e
hrguru2017 <surinder.hrgp@gmail.com>
2025-06-06 11:11:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -371,11 +371,11 @@ def profile_history():
                            pagination=pagination)
 
 @main_bp.route('/users/create', methods=['GET', 'POST'])
-#@login_required
+@login_required
 def create_user():
-    #if current_user.role != 'manager':
-    #    flash('You do not have permission to perform this action.', 'danger')
-    #    return redirect(url_for('main.dashboard'))
+    if current_user.role != 'manager':
+        flash('You do not have permission to perform this action.', 'danger')
+        return redirect(url_for('main.dashboard'))
     form = UserCreationForm()
     if form.validate_on_submit():
         username = form.username.data
